f0b047a23505a8a476f4efa22c0c0e615f83f88b
Mauricio <llanosmauricio10@gmail.com>
2025-04-13 15:03:45 -0500

    second commit

--- example/lib/presentation/screens/templates/cart_template_screen.dart
+++ example/lib/presentation/screens/templates/cart_template_screen.dart
@@ -1,5 +1,10 @@
-import 'package:fake_store_design/design_system.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:example/config/mock/product_mock.dart';
+import 'package:example/presentation/providers/cart_provider.dart';
+import 'package:fake_store_api_package/domain/models/product_entity.dart';
+import 'package:fake_store_design/design_system.dart';
+
 import 'package:go_router/go_router.dart';
 
 class CartTemplateScreen extends StatefulWidget {
@@ -19,24 +24,52 @@ class _CartTemplateScreenState extends State<CartTemplateScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return CartTemplate(authentication: authentication, listCart: products);
+  }
+}
+
+class CartTemplate extends StatelessWidget {
+  const CartTemplate({
+    super.key,
+    required this.authentication,
+    required this.listCart,
+    this.name,
+    this.lastName,
+    this.backonPressed,
+    this.logOutonPressed,
+    this.logInonPressed,
+    this.onPressedminus,
+    this.onPressedplus,
+    this.onDialogButtonPressed,
+  });
+
+  final bool authentication;
+  final String? name;
+  final String? lastName;
+  final List<Product> listCart;
+  final VoidCallback? backonPressed;
+  final VoidCallback? logOutonPressed;
+  final VoidCallback? logInonPressed;
+  final VoidCallback? onPressedminus;
+  final VoidCallback? onPressedplus;
+  final VoidCallback? onDialogButtonPressed;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar:
           authentication
               ? CustomAppbars(
                 appbarType: AppbarType.cartAppbarlogout,
-                backonPressed: () {},
-                logOutonPressed: () {
-                  _handleAuthentication();
-                },
-                name: 'Pepito',
-                lastName: 'Perez',
+                backonPressed: backonPressed,
+                logOutonPressed: logOutonPressed,
+                name: name,
+                lastName: lastName,
               )
               : CustomAppbars(
                 appbarType: AppbarType.cartAppbarlogin,
-                backonPressed: () {},
-                logInonPressed: () {
-                  _handleAuthentication();
-                },
+                backonPressed: backonPressed,
+                logInonPressed: logInonPressed,
               ),
       backgroundColor: Color.fromARGB(255, 238, 238, 238),
       body: Padding(
@@ -44,16 +77,16 @@ class _CartTemplateScreenState extends State<CartTemplateScreen> {
         child: Stack(
           children: [
             ListView.separated(
-              itemCount: 20,
+              itemCount: listCart.length,
               itemBuilder: (BuildContext context, int index) {
+                final product = listCart[index];
                 return ProductCartContainer(
-                  url:
-                      'https://ih1.redbubble.net/image.4905811472.8675/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
-                  productName: 'productName',
+                  url: product.image,
+                  productName: product.title,
                   amount: 'x1',
-                  productPrice: '1200',
-                  onPressedminus: () {},
-                  onPressedplus: () {},
+                  productPrice: product.price.toString(),
+                  onPressedminus: onPressedminus,
+                  onPressedplus: onPressedplus,
                 );
               },
               separatorBuilder:
@@ -68,14 +101,11 @@ class _CartTemplateScreenState extends State<CartTemplateScreen> {
                     authentication
                         ? CustomDialog(
                           dialogType: DialogType.authenticated,
-                          onDialogButtonPressed: () {},
+                          onDialogButtonPressed: onDialogButtonPressed,
                         )
                         : CustomDialog(
                           dialogType: DialogType.unauthenticated,
-                          onDialogButtonPressed: () {
-                            _handleAuthentication();
-                            context.pop();
-                          },
+                          onDialogButtonPressed: onDialogButtonPressed,
                         ),
               ),
             ),
